18118d5cd60f513e21e22145c2e3e13b61244dda
LISA <lixaguseva0908@gmail.com>
2022-10-21 22:40:37 +0300

    LocalizationTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/rrtuners/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/rrtuners/LocalizationTest.java
@@ -3,9 +3,12 @@ package org.firstinspires.ftc.teamcode.drive.rrtuners;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.modules.Lift;
 
 /**
  * This is a simple teleop routine for testing localization. Drive the robot around like a normal
@@ -16,23 +19,82 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  */
 @TeleOp(group = "drive")
 public class LocalizationTest extends LinearOpMode {
+    DcMotor StCh;
+    CRServo servo3;
+    CRServo servo2;
+    double kp = 0;
+    double ki = 0;
+    double kd = 0;
+    double ref = 0;
+    double kv = 0.1;
+    double prevTime = 0;
+    double prevError = 0;
+
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Lift lift = new Lift(this);
+
+        StCh = hardwareMap.get(DcMotor.class, "StaticChain");
+        StCh.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        StCh.setDirection(DcMotorSimple.Direction.FORWARD);
+        servo2 = hardwareMap.get(CRServo.class, "servo2");
+        servo3 = hardwareMap.get(CRServo.class, "servo3");
+
+
 
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
         while (!isStopRequested()) {
+
+            double err = ref - StCh.getCurrentPosition();
+            double P = kp * err;
+            double I = ki * err * (time - prevTime);
+            double D = kd * (err - prevError) / (time - prevTime);
+            prevError = err;
+            prevTime = time;
+
             drive.setWeightedDrivePower(
                     new Pose2d(
+                            - gamepad1.left_stick_y
                             -gamepad1.left_stick_x,
-                            -(-gamepad1.left_stick_y),
-                            -gamepad1.right_stick_x
+                            -(gamepad1.right_trigger - gamepad1.left_trigger)
+
                     )
             );
 
+            if(gamepad2.right_stick_y <0.1 && gamepad2.right_stick_y >= 0){
+                StCh.setPower(0);
+            }
+            else{
+                StCh.setPower(gamepad2.right_stick_y);
+            }
+
+
+            if(gamepad2.a) {
+                while (gamepad2.a) {
+                    double power = 1;
+                    servo3.setPower(power);
+                    servo2.setPower(-power);
+                }
+            }
+            if(gamepad2.b) {
+                while (gamepad2.b) {
+                    double power = 1;
+                    servo3.setPower(-power);
+                    servo2.setPower(power);
+                }
+            }
+            else {
+                servo3.setPower(0);
+                servo2.setPower(0);
+            }
+
+            if(err > 30){
+                StCh.setPower(gamepad2.right_stick_y * kv);
+            }
             drive.update();
 
             Pose2d poseEstimate = drive.getPoseEstimate();
